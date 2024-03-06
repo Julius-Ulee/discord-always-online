1fcf767f63d395e4731c016e0e2e95e2d9c4d89d
Azriel Fidzlie <61336116+Julius-Ulee@users.noreply.github.com>
2024-03-06 13:20:26 +0700

    🚀 chore: change little bit

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,11 @@
-var http = require('http');
+const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('schoolprograms.my.id');
-}).listen(8080);
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
