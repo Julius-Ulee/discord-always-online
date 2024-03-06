5dd3bdb17453d10a055f9332f8eb154bcb153550
Azriel Fidzlie <61336116+Julius-Ulee@users.noreply.github.com>
2024-03-06 13:15:41 +0700

    🚀 chore: change little bit

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.write("schoolprograms.my.id");
-  res.end();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('schoolprograms.my.id');
 }).listen(8080);
